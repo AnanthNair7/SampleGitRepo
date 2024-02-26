64ef5c802b765200f3d43a5d72eda262ec7528ff
AnanthNair7 <ananth.nair@icloud.com>
2024-02-26 15:41:01 +0530

    added print Func

---
+++ gitSample/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  gitSample
+//
+//  Created by Ananth Nair on 26/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func printName(){
+        print("Hello git")
+    }
+}
+
